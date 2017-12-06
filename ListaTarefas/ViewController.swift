12d33f50aa56a2869f1bb39aaaac35e011d9221f
Dali Freire <dalifreire@gmail.com>
2017-12-06 18:06:42 -0200

    Initial Commit

---
+++ ListaTarefas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ListaTarefas
+//
+//  Created by ALOC SP08156 on 06/12/2017.
+//  Copyright © 2017 ALOC SP08156. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
